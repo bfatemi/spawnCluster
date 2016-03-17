e480de4492fb083323148f4c64ac06e5dfa71a83
ben105 <ben_rooke@icloud.com>
2016-03-17 09:09:21 -0700

    Safely update the hostname entries in hosts file.
    I was thinking that this code might cause duplicate entries in the hosts file if the hostname already exists. This might be bad if the IP addresses differ. So I added some logic to try and do a find and replace first, and if there is no existing hostname then we will append it to the hosts file.

--- mapNodes.sh
+++ mapNodes.sh
@@ -2,11 +2,25 @@
 
 # createCluster.sh
 
+# Location of the hosts file on Linux and Mac systems.
+hosts_file=/etc/hosts
+# Regular expression to find the IP address, at the start of a line.
+regex='^([0-9]+\.){3}[0-9]+'
+
+update_host () {
+  # Search for the existence of the hostname in the file.
+  egrep "$regex $1" $hosts_file > /dev/null && \
+  # And replace the IP address with the provided one, if found.
+  { sed -i -r "s,${regex}( ${1}),$2\2,g" $hosts_file; return 0; } || \
+  # Else, append a new entry.
+  echo "$2 $1" >> $hosts_file
+}
+
 # AT THIS POINT WE NEED TO HAVE THESE SLAVE NODES UP AND RUNNING
 echo "...Updating /etc/hosts to define master and slave network"
-printf "\n\n\n%s" "45.55.115.231 master" >> /etc/hosts
-printf "\n%s" "45.55.115.232 slave1" >> /etc/hosts
-printf "\n%s" "45.55.115.233 slave2" >> /etc/hosts
+update_host master 45.55.115.231 
+update_host slave1 45.55.115.232
+update_host slave2 45.55.115.233
 
 #echo "...Sending ssh public keys to slave nodes"
 #ssh-copy-id -i slave1
